dc3f59b6f1534e088bc097789148697e28cddc28
qcloud <ubuntu@localhost.localdomain>
2025-12-31 22:09:20 +0800

    bugfix: kling api error info

--- router/web-router.go
+++ router/web-router.go
@@ -19,9 +19,15 @@ func SetWebRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
 	router.Use(middleware.Cache())
 	router.Use(static.Serve("/", common.EmbedFolder(buildFS, "web/dist")))
 	router.NoRoute(func(c *gin.Context) {
-		if strings.HasPrefix(c.Request.RequestURI, "/v1") || strings.HasPrefix(c.Request.RequestURI, "/api") || strings.HasPrefix(c.Request.RequestURI, "/assets") {
-			controller.RelayNotFound(c)
-			return
+		apiPrefixes := []string{
+			"/v1", "/v1beta", "/api", "/assets",
+			"/kling", "/jimeng", "/pg", "/mj", "/suno",
+		}
+		for _, prefix := range apiPrefixes {
+			if strings.HasPrefix(c.Request.RequestURI, prefix) {
+				controller.RelayNotFound(c)
+				return
+			}
 		}
 		c.Header("Cache-Control", "no-cache")
 		c.Data(http.StatusOK, "text/html; charset=utf-8", indexPage)
